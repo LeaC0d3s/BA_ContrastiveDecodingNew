5646e56e6e5f2e7898e2a4229535d030ed159b36
LeaC0d3s <75457695+LeaC0d3s@users.noreply.github.com>
2024-04-08 23:49:28 +0200

    remove unused variable

--- translation_models/llama.py
+++ translation_models/llama.py
@@ -219,8 +219,7 @@ class LLaMaTranslationModel(TranslationModel):
         logits_processor = LogitsProcessorList([
             EnsembleLogitsProcessor(num_beams=num_beams, source_weights=src_weights),
         ])
-        logging.info("input_ids shape:", input_ids)
-        logging.info("input_ids[0] shape:", input_ids_1)
+
 
         outputs = self.model.generate(
             input_ids=input_ids,
