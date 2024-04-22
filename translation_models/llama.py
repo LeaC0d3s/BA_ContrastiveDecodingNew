6df1d17ddeadf9a12dc4c7c027ebdb9e4f53032a
LeaC0d3s <75457695+LeaC0d3s@users.noreply.github.com>
2024-04-23 00:56:43 +0200

    fix past_key_value

--- translation_models/llama.py
+++ translation_models/llama.py
@@ -291,6 +291,7 @@ class LLaMaTranslationModel(TranslationModel):
         print(outputs_german.past_key_values[0][0])
         print(len(outputs_german.past_key_values[0]))
         print(len(outputs_german.past_key_values[0][0]))
+        print(outputs_german.past_key_values[0][0])
 
         # Initialize empty list to store English translations
         english_translations = []
