6dd93abc27e4b59a8013ab84d10dd77fd1f727b7
LeaC0d3s <75457695+LeaC0d3s@users.noreply.github.com>
2024-05-31 16:01:21 +0200

    remove files

--- Table_Processing/average_prob_trend.py
+++ Table_Processing/average_prob_trend.py
@@ -22,28 +22,26 @@ table.columns = [
 
 
 # Filter the data to include only the specified indices that contain NoT errors
-selected_indices_b = [90, 281, 533, 863, 873, 875, 92, 273, 505, 532, 683,824,826,
-                    830,853,874, 956, 1, 133, 134, 173, 245, 296,
-                    311, 375, 422, 484, 494, 558, 559, 629, 655, 740, 871, 889, 891, 934]
+selected_indices_b = [90, 281, 533, 863, 873, 875, 92, 273, 505, 532, 683, 824, 826, 830,853,874, 956, 1, 133, 134, 173, 245, 296, 311, 375, 422, 484, 494, 558, 559, 629, 655, 740, 871, 889, 891, 934]
 selected_indices_cd5 = [90, 863, 873, 875, 683, 824, 826, 830, 853, 874]
 selected_indices_cd9 = [90, 863, 873, 875]
 error_type = "NoT"
 # Filter the data to include only the specified indices that contain PartT errors
-selected_indices_b = [490,679,107,172,474, 615,847,914,154, 196, 754, 877]
-selected_indices_cd5 = [490,533,679,107,172,474, 615,847, 914, 12, 327, 974, 217, 444, 702]
-selected_indices_cd9 = [490,533,679,956, 12, 327, 974, 121, 210,365,387, 614, 772, 851, 893]
+selected_indices_b = [490, 679, 107, 172, 474, 615, 847, 914, 154, 196, 754, 877]
+selected_indices_cd5 = [490, 533, 679, 107, 172, 474, 615,847, 914, 12, 327, 974, 217, 444, 702]
+selected_indices_cd9 = [490, 533, 679, 956, 12, 327, 974, 121, 210, 365, 387, 614, 772, 851, 893]
 error_type = "PartT"
 
 # Filter the data to include only the specified indices that contain Insert and or NonW errors
 selected_indices_b = [467, 444, 702, 772, 893]
 selected_indices_cd5 = [281, 467, 154, 196, 558, 559, 629,655, 754,871,877, 889, 934, 772, 893]
-selected_indices_cd9 = [281, 92, 107, 172,467,474, 505, 615, 824, 826, 847, 853, 874, 936, 196, 375, 558, 559, 629, 655,754,871,889, 934,444,702, 41, 469]
+selected_indices_cd9 = [281, 92, 107, 172,467,474, 505, 615, 824, 826, 847, 853, 874, 936, 196, 375, 558, 559, 629, 655, 754, 871, 889, 934, 444, 702, 41, 469]
 error_type = "Insert_NonW_mix"
 
 # Filter the data to include only the specified indices that contain no Error Type
 selected_indices_b = [12, 327, 974,217, 41, 121, 210, 365, 387, 469, 614, 851]
-selected_indices_cd5 = [956, 2, 133, 134, 173, 245,296,311,375, 422, 484, 494, 740, 891, 41, 121, 210, 365, 387, 469, 614, 851]
-selected_indices_cd9 = [273, 532, 830, 914,2, 133, 134, 154, 173, 245,296,311,422, 484, 494, 740, 877, 891, 217]
+selected_indices_cd5 = [956, 2, 133, 134, 173, 245, 296, 311, 375, 422, 484, 494, 740, 891, 41, 121, 210, 365, 387, 469, 614, 851]
+selected_indices_cd9 = [273, 532, 830, 914,2, 133, 134, 154, 173, 245, 296, 311, 422, 484, 494, 740, 877, 891, 217]
 error_type = "No"
 
 
